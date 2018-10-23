1a945aec3fbb1c9548a8f7e713073ae9d4b3981e
annaodziemczyk <anna.odziemczyk@mycit.ie>
2018-10-23 20:11:37 +0100

    spring boot starter project

---
+++ src/main/java/com/cit/RfidclonespyApplication.java
@@ -0,0 +1,12 @@
+package com.cit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RfidclonespyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RfidclonespyApplication.class, args);
+	}
+}
