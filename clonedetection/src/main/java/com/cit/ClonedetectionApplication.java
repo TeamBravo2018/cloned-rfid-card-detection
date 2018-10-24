6ff95c7faa0855a5cb3dcc648b905e280668a180
annaodziemczyk <anna.odziemczyk@mycit.ie>
2018-10-24 19:50:25 +0100

    adding some modules

---
+++ clonedetection/src/main/java/com/cit/ClonedetectionApplication.java
@@ -0,0 +1,12 @@
+package com.cit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClonedetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClonedetectionApplication.class, args);
+	}
+}
--- src/test/java/com/cit/RfidclonespyApplicationTests.java
+++ src/test/java/com/cit/RfidclonespyApplicationTests.java
@@ -1,13 +1,45 @@
 package com.cit;
 
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+import org.apache.cassandra.exceptions.ConfigurationException;
+import org.apache.thrift.transport.TTransportException;
+import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.IOException;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RfidclonespyApplicationTests {
+
+	@BeforeClass
+	public static void startCassandraEmbedded() {
+		try {
+			EmbeddedCassandraServerHelper.startEmbeddedCassandra();
+		} catch (TTransportException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ConfigurationException e) {
+			e.printStackTrace();
+		}
+		Cluster cluster = Cluster.builder()
+				.addContactPoints("127.0.0.1").withPort(9142).build();
+		Session session = cluster.connect();
+	}
+
+	@AfterClass
+	public static void stopCassandraEmbedded() {
+		EmbeddedCassandraServerHelper.cleanEmbeddedCassandra();
+	}
 
 	@Test
 	public void contextLoads() {
