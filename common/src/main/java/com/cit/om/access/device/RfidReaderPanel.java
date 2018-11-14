b6519aa25e848ac697992ff1e250c5bd9b3571d7
annaodziemczyk <anna.odziemczyk@mycit.ie>
2018-11-14 18:33:27 +0000

    Project structure

---
+++ common/src/main/java/com/cit/om/access/device/RfidReaderPanel.java
@@ -0,0 +1,7 @@
+package com.cit.om.access.device;
+
+/**
+ * Created by odziea on 11/12/2018.
+ */
+public class RfidReaderPanel {
+}
---
+++ common/src/main/java/com/cit/om/access/request/AccessRequest.java
@@ -0,0 +1,7 @@
+package com.cit.om.access.request;
+
+/**
+ * Created by odziea on 11/12/2018.
+ */
+public class AccessRequest {
+}
