3eb7cd7c85d4637e8788450eb27e987e08d871f5
annaodziemczyk <anna.odziemczyk@mycit.ie>
2018-11-17 16:33:43 +0000

    Project structure

---
+++ locator/src/main/java/com/cit/locator/panellocator/IPanelLocatorService.java
@@ -0,0 +1,7 @@
+package com.cit.locator.panellocator;
+
+/**
+ * Created by odziea on 11/14/2018.
+ */
+public interface IPanelLocatorService {
+}
---
+++ locator/src/main/java/com/cit/locator/panellocator/PanelLocatorService.java
@@ -0,0 +1,13 @@
+package com.cit.locator.panellocator;
+
+import com.cit.common.manager.RestTemplateMapper;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by odziea on 11/14/2018.
+ */
+@Service
+public class PanelLocatorService implements IPanelLocatorService {
+
+    private RestTemplateMapper restTemplateMapper;
+}
